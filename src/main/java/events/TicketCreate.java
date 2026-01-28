fad12a7e8b633ddafb3070f76274ad975166abe9
Ellie Bacon <bacon.e@northeastern.edu>
2026-01-27 21:30:25 -0500

    Add thread revealed event handling for ticket creation

--- src/main/java/events/TicketCreate.java
+++ src/main/java/events/TicketCreate.java
@@ -7,6 +7,30 @@ import net.dv8tion.jda.api.events.channel.ChannelCreateEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class TicketCreate extends ListenerAdapter {
+	@Override
+	public void onThreadRevealed(ThreadRevealedEvent e) {
+		System.out.println("=== THREAD REVEALED EVENT FIRED ===");
+		ThreadChannel channel = e.getThread();
+		System.out.println("Thread ID: " + channel.getId());
+		System.out.println("Thread name: " + channel.getName());
+		System.out.println("Parent ID: " + (channel.getParentChannel() != null ? channel.getParentChannel().getId() : "NULL"));
+		System.out.println("Expected forum ID: " + Main.getForumChannelID());
+		System.out.println("isForumChannel result: " + Utils.isForumChannel(channel));
+		
+		if(Utils.isForumChannel(channel)) {
+			System.out.println("Creating ticket in thread: " + channel.getName());
+			Utils.editPost(channel, "", "open", "");
+			String message = """
+			**OPEN TICKET**
+			TAs: To claim this post, run `!claim` in this channel.
+			OP: To close this post, run `!close` in this channel.
+			""";
+			Message sentMessage = channel.sendMessage(message).complete();
+			sentMessage.pin().queue();
+			System.out.println("Ticket created successfully!");
+		}
+	}
+	/*
 	public void onChannelCreate(ChannelCreateEvent e) {
 		System.out.println("=== CHANNEL CREATE EVENT FIRED ===");
 		System.out.println("Channel type: " + e.getChannel().getType());
@@ -24,4 +48,5 @@ public class TicketCreate extends ListenerAdapter {
 			sentMessage.pin().queue();
 		}
 	}
+	*/
 }
